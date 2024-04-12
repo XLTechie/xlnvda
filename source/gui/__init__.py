535f5097a20b50fbc7d0093ff4eee6537893a752
Luke Davis <XLTechie+github@newanswertech.com>
2024-04-12 03:34:15 -0400

    Change return values checked.

--- source/gui/__init__.py
+++ source/gui/__init__.py
@@ -454,7 +454,7 @@ This tool is used by NVDA to fix problems it may have as it tries to interact wi
 	It examines the system registry for corrupted or missing accessibility entries and will correct them.\n
 Those entries can sometimes be damaged by installing or uninstalling programs, or other system events. This can result in "unknown" or "pane" being spoken instead of the content you were expecting, or previously accessible elements suddenly no longer reading correctly.\n\n
 		You have most likely been asked to run this tool by NVDA support or a power user trying to assist you.\n
-Because it needs to modify the Windows registry, if you have User Account Control (UAC) active, you will be prompted by UAC before this tool can do its job. This is normal and you should answer using the Continue button.\n\n
+Because it needs to modify the Windows registry, if you have User Account Control (UAC) active, you will be prompted by UAC before this tool can do its job. This is normal and you should answer by pressing the yes button.\n\n
 Do you wish to try to repair the registry now?\n"""  # noqa: E501 Flake8 sees this block as one line
 		)
 		# Translators: The title of various dialogs displayed when using the COM Registration Fixing tool 
@@ -486,7 +486,7 @@ Do you wish to try to repair the registry now?\n"""  # noqa: E501 Flake8 sees th
 			style=wx.YES_NO | wx.CANCEL | wx.STAY_ON_TOP,
 			parent=self
 		)
-		if response == wx.ID_CANCEL or response == wx.ID_NO:
+		if response == wx.CANCEL or response == wx.NO:
 			log.debug("Run of COM Registration Fixing Tool canceled before UAC.")
 			return
 		progressDialog = IndeterminateProgressDialog(
